65212dd19c3e4110a2ca870f7bc6b6462618c3fb
Lagunaswift <jamesswift1988@gmail.com>
2025-07-18 19:01:45 +0100

    Update index.ts

--- supabase/functions/wix-webhook-handler/index.ts
+++ supabase/functions/wix-webhook-handler/index.ts
@@ -1,3 +1,4 @@
+//studio/supabase/functions/wix-webhook-handler/index.ts
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
