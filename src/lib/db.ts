53faf61a63948c54b40071ed5c321405450041ce
James Swift <jamesswift1988@gmail.com>
2025-07-22 16:29:59 +0000

    next pahse if thats all from phase two

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,23 +14,7 @@ export class AppDatabase extends Dexie {
   dailyManualMacrosLog!: Table<DailyManualMacrosLog>;
 
   constructor() {
-    super('MealPlannerProDB_v3'); // Increment version for schema change
-    this.version(2).stores({ // New version with syncStatus index
-      recipes: '++id, name, *tags, isCustom', 
-      plannedMeals: 'id, date, mealType, recipeId, syncStatus', // Add syncStatus
-      pantryItems: 'id, name, category, syncStatus', // Add syncStatus
-      dailyWeightLog: 'date, syncStatus', // Add syncStatus
-      userProfile: 'id, syncStatus', // Add syncStatus
-      dailyVitalsLog: 'date, syncStatus', // Add syncStatus
-      dailyManualMacrosLog: 'id, date, syncStatus' // Add id as primary key & syncStatus
-    }).upgrade(tx => {
-       // This upgrade function will only run if the user had version 1.
-       // It's a good place to migrate data if needed, but for now, we'll just let Dexie handle the schema update.
-       console.log("Upgrading Dexie schema from v1 to v2");
-    });
-    // This is the latest schema definition.
-    // If a user has v1, it will be upgraded by the function above.
-    // If a user has no database, this v2 schema will be created.
+    super('MealPlannerProDB_v3');
     this.version(1).stores({ 
         recipes: '++id, name, *tags, isCustom', 
         plannedMeals: 'id, date, mealType, recipeId',
@@ -73,7 +57,7 @@ export async function getOrCreateUserProfile(userId: string): Promise<UserProfil
       age: null,
       sex: null,
       activityLevel: 'notSpecified',
-      trainingExperienceLevel: 'notSpecified',
+      training_experience_level: 'notSpecified',
       bodyFatPercentage: null,
       athleteType: 'notSpecified',
       primaryGoal: 'notSpecified',
@@ -81,16 +65,15 @@ export async function getOrCreateUserProfile(userId: string): Promise<UserProfil
       leanBodyMassKg: null,
       rda: null,
       subscription_status: 'none',
-      hasAcceptedTerms: false,
-      lastCheckInDate: null,
-      targetWeightChangeRateKg: null,
+      has_accepted_terms: false,
+      last_check_in_date: null,
+      target_weight_change_rate_kg: null,
       dashboardSettings: { showMacros: true, showMenu: true, showFeaturedRecipe: true, showQuickRecipes: true },
       // Initialize log arrays as empty
       dailyWeightLog: [],
       dailyVitalsLog: [],
       dailyManualMacrosLog: [],
       favorite_recipe_ids: [],
-      syncStatus: 'synced',
   };
 
   try {
